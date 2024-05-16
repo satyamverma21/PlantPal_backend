30adaeed23da26e07c4c30f82b30cf380de40571
satyamverma21 <satyamverma2122004@gmail.com>
2024-05-16 14:16:50 +0530

    completed: diceaseId

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public/images'));
 app.use(cors())
 
+const production = false
+
 let fname;
 // const storage = multer.diskStorage({
 //     destination: (req, file, cb) => {
@@ -71,33 +73,117 @@ app.post('/idPlant', upload.single('image'), async (req, res) => {
     const url = 'https://plant.id/api/v3/identification';
 
     try {
-        // const response = await axios.post(url, {
-        //     images: [Buffer.from(file.buffer).toString('base64')],
-        // }, {
-        //     headers: {
-        //         'Api-Key': `${process.env.idKey}`,
-        //         'Content-Type': 'application/json',
-        //     },
-        //     params: {
-        //         details: 'common_names,url,description,image,synonyms,edible_parts,watering'
-        //     }
-        // })
 
-        try {
-            const data = fs.readFileSync('plantId.json', 'utf8');
-            const response = JSON.parse(data);
-            res.json({ data: response.result.classification.suggestions })
+        if (production) {
 
-        } catch (error) {
-            console.error('Error reading file:', error);
+            const response = await axios.post(url, {
+                images: [Buffer.from(file.buffer).toString('base64')],
+            }, {
+                headers: {
+                    'Api-Key': `${process.env.idKey}`,
+                    'Content-Type': 'application/json',
+                },
+                params: {
+                    details: 'common_names,url,description,image,synonyms,edible_parts,watering'
+                }
+            })
+
+            if (response.data.result.is_plant.binary)
+                res.json({ data: response.data.result.classification.suggestions })
+            else
+                res.json({ msg: "Could not detect plant." })
+
+        } else {
+
+            try {
+                const response = fs.readFileSync('plantId.json', 'utf8');
+                const data = JSON.parse(response);
+
+                if (data.result.is_plant.binary)
+                    res.json({ data: data.result.classification.suggestions })
+                else
+                    res.json({ msg: "Could not detect plant." })
+
+            } catch (error) {
+                console.error('Error reading file:', error);
+            }
+
+            // try {
+            //     await fs.writeFile('response.json', response.data.result);
+            //     console.log(`API response written to file`);
+            // } catch (error) {
+            //     console.error('Error writing file:', error);
+            // }
+
+            // console.log("debug: /idPlant responded ", response)
+            // res.json({ data: response.data.result.classification.suggestions })
         }
-
-
-        console.log("debug: /idPlant responded ")
 
     } catch (error) {
         res.json({ msg: error.message })
         console.log("debug: /idPlant error", error.message)
+    }
+})
+
+app.post('/idDicease', upload.single('image'), async (req, res) => {
+
+    console.log("debug: /idDicease ")
+
+    const file = req.file;
+    const url = 'https://plant.id/api/v3/health_assessment';
+
+    try {
+        if (production) {
+            const response = await axios.post(url, {
+                images: [Buffer.from(file.buffer).toString('base64')],
+            }, {
+                headers: {
+                    'Api-Key': `${process.env.idKey}`,
+                    'Content-Type': 'application/json',
+                },
+                params: {
+                    details: 'local_name,description,url,treatment,classification,common_names,cause'
+                }
+            })
+
+            if (response.data.result.is_plant.binary)
+                res.json({ data: response.data.result })
+            else
+                res.json({ msg: "Could not detect plant." })
+        }
+        else {
+
+            try {
+                const response = fs.readFileSync('diceaseId.json', 'utf8');
+                const data = JSON.parse(response);
+                if (data.result.is_plant.binary)
+                    res.json({ data: data.result })
+                else
+                    res.json({ msg: "Could not detect plant." })
+
+            } catch (error) {
+                console.error('Error reading file:', error);
+            }
+
+            // const jsonData = JSON.stringify(response.data, null, 2); // Format JSON with indentation
+
+            // fs.writeFile('response_capture.json', jsonData, (err) => {
+            //     if (err) {
+            //         console.error('Error writing to file:', err);
+            //     } else {
+            //         console.log('Response successfully dumped to response.json');
+            //     }
+            // })
+
+
+            // console.log("debug: /idDicease responded ", response)
+            // res.json({ data: response.data.result.classification.suggestions })
+
+        }
+
+    } catch (error) {
+        res.json({ msg: error.message })
+        console.log("debug: /idDicease error", error.message)
     }
 })
 
